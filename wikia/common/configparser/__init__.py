cea1fd698b35f6b690ef8160272aaf93c2a8d662
Jonathan Robson <jnrbsn@gmail.com>
2014-06-30 15:01:43 -0700

    Import exception classes from configparser.

--- wikia/common/configparser/__init__.py
+++ wikia/common/configparser/__init__.py
@@ -6,8 +6,14 @@ Wrapper around the configparser backport from Python 3.2+:
 https://pypi.python.org/pypi/configparser
 """
 
+from __future__ import absolute_import
 import json
 import pkg_resources
+
+from configparser import (Error, NoSectionError, DuplicateSectionError, DuplicateOptionError,
+                          NoOptionError, InterpolationError, InterpolationDepthError,
+                          InterpolationMissingOptionError, InterpolationSyntaxError,
+                          MissingSectionHeaderError, ParsingError)
 
 from .configparser import ConfigParser, ExtendedInterpolation
 
