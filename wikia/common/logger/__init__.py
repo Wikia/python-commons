65b6289b0fbf2ec31dd4ce0f476883911b22442e
Krystian Mistrzak <krystian.mistrzak@wikia-inc.com>
2018-04-27 18:03:14 +0200

    Update __init__.py

--- wikia/common/logger/__init__.py
+++ wikia/common/logger/__init__.py
@@ -8,6 +8,6 @@ Common logging classes for centralized logging at Wikia
 import json
 import pkg_resources
 
-from .logger import Logger, LogFormatter, LogRecord, WikiaJsonLogsFormatter, configure
+from .logger import Logger, LogFormatter, LogRecord
 
 __version__ = json.loads(pkg_resources.resource_string(__name__, 'build.json'))['version']
